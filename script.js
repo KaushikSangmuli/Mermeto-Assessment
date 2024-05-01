d004922ed6a90b3be91b336b97e6f14b80f4b441
kaushik sangmuli <sangmulekoushik@gmail.com>
2024-05-02 03:20:16 +0530

    comments added in scrip.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const colorBoxes = document.querySelectorAll(".box")
 const colorBorder = document.querySelectorAll(".color-border")
 const discription = document.querySelector("#discription-text")
 
-let bgColor;
+let bgColor;  
 let isColorSelected = false;
 let isSizeSelected = false;
 
@@ -14,29 +14,27 @@ fetch(" https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.jso
 .then((data)=>{
     return data.json()
 }).then((data)=>{
-    
     return data.product
 }).then((data)=>{
     vendor.innerText = data.vendor
     productName.innerText =data.title
     
-    let finalPrice = parseFloat(data.price.replace("$", ""))
+    let finalPrice = parseFloat(data.price.replace("$", ""))  // convert value from string to number.
     let dscntPrice = parseFloat(data.compare_at_price.replace("$",""))
    
     price.innerHTML = `<span id="product-price"> $${finalPrice}</span> <span id="prcnt-discount">
-    ${100-((finalPrice/dscntPrice)*100).toFixed(0)}%off</span>`;
+    ${100-((finalPrice/dscntPrice)*100).toFixed(0)}%off</span>`;   //automatically calculate the % off.
     originalPrice.innerText = `$${dscntPrice}`
 
     colorBoxes.forEach((box, index)=>{
         let color = Object.values(data.options[0].values[index])
-        box.style.backgroundColor = color
+        box.style.backgroundColor = color   
     })
+
     colorBorder.forEach((border,index)=>{
-
         border.addEventListener("click", ()=>{
-
             colorBorder.forEach((border)=>{
-                border.style.borderColor = ""
+                border.style.borderColor = ""     // remove the previous border from color button.
             })
             let color = Object.values(data.options[0].values[index])
             border.style.borderColor = color
@@ -47,14 +45,13 @@ fetch(" https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.jso
     colorBoxes.forEach((box, index)=>{
         box.addEventListener("click",()=>{
             bgColor = Object.keys(data.options[0].values[index])
-            
-            isColorSelected =true
+            isColorSelected =true       // to make sure that color is selected
         })
     })
 
     const dscrpt =  data.description
-    const starting = dscrpt.indexOf(">") + ">".length
-    const ending = dscrpt.indexOf("</p>")
+    const starting = dscrpt.indexOf(">") + ">".length  //customize the text starting from value.
+    const ending = dscrpt.indexOf("</p>")         //customize the text ending from value.
     const dsc = dscrpt.substring(starting , ending)
     discription.innerText = dsc
 
@@ -62,15 +59,11 @@ fetch(" https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.jso
 let reqcolor;
 colorBoxes.forEach((box) => {
     const img = box.querySelector(".tick-mark");
-    
     box.addEventListener("click", () => {
-        // Hide all images
         colorBoxes.forEach((box) => {
-            box.querySelector(".tick-mark").classList.add("hidden");
+            box.querySelector(".tick-mark").classList.add("hidden");    // Hide all images
         });
-        // Show the image inside the clicked box
         img.classList.remove("hidden");
-       
         isColorSelected =true
 
     });
@@ -81,7 +74,7 @@ let selectedSize;
 sizes.forEach((size) =>{
     size.addEventListener("click", ()=>{
          selectedSize = size.value
-         isSizeSelected = true
+         isSizeSelected = true   //make sure that size is selected
     })
 })
 
@@ -95,14 +88,12 @@ minus.addEventListener("click",()=>{
     if (count>1){
         count--
         noOFItems.innerText = count
-   
     }
 })
 
 plus.addEventListener("click", ()=>{
     count++
     noOFItems.innerText = count
-   
 })
 
 const result = document.querySelector("#status")
@@ -110,11 +101,12 @@ const addToCart = document.querySelector("#cart-btn")
 addToCart.addEventListener("click",()=>{
     result.classList.remove("hidden")
     if(!isColorSelected){
-        result.innerText = ` select the Color for product`
+        result.innerText = ` choose the Color for product`
+        result.style.color="red"
     } else if(!isSizeSelected){
         result.innerText = ` select the Size for product`
+        result.style.color="red"
     } else if( isColorSelected && isSizeSelected ){
         result.innerText = ` ${productName.innerText} with Color ${bgColor} and Size ${selectedSize}  added to cart`
-     
     } 
 })
